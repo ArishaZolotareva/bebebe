d683256162ded3ff0357d501d0b134d582c9abf5
ArishaZolotareva <245zolotoarishka@gmail.com>
2023-09-22 17:51:50 +0400

    made beutifully "contacts"

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,25 +1,73 @@
 <template>
-    <div>
-      <v-container
-        class="bg-surface-variant mb-6"
-      >
-        <v-row
-          align="center"
-          no-gutters
-          style="height: 50px;"
-        >
-          <v-col
-            v-for="n in 3"
-            :key="n"
-          >
-            <v-sheet class="pa-2 ma-2">
-              contacti
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-container>
+      <v-row class="bg-grey">
+        <v-col cols="12">
+          <v-sheet class="bg-grey
+          pa-2 ma-2">
+            text tipa vi mozete sviazatsia c nami
+          </v-sheet>
+
+          <v-sheet class="pa-2 ma-2">
+            contacti
+          </v-sheet>
   
+          <v-row no-gutters class="bg-black">
+            <v-col
+              cols="8"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa name
+              </v-sheet>
+            </v-col>
+  
+            <v-col
+              cols="4"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa number
+              </v-sheet>
+            </v-col>
+          </v-row>
 
-    </div>
+          <v-row no-gutters class="bg-black">
+            <v-col
+              cols="8"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa name
+              </v-sheet>
+            </v-col>
+  
+            <v-col
+              cols="4"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa number
+              </v-sheet>
+            </v-col>
+          </v-row>
+
+          <v-row no-gutters class="bg-black">
+            <v-col
+              cols="8"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa name
+              </v-sheet>
+            </v-col>
+  
+            <v-col
+              cols="4"
+            >
+              <v-sheet class="pa-2 ma-2">
+                tipa number
+              </v-sheet>
+            </v-col>
+          </v-row>
+
+        </v-col>
+  
+        
+      </v-row>
+    </v-container>
   </template>
-
