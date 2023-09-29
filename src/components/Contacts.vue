7c0641224b2aa370d0c58c36ed4eca142961c6e7
ArishaZolotareva <245zolotoarishka@gmail.com>
2023-09-29 17:53:01 +0400

    contacti

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,61 +1,53 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-sheet class="
-          pa-2 ma-2">
-          text tipa vi mozete sviazatsia c nami
-        </v-sheet>
-
-        <v-sheet class="pa-2 ma-2">
-          contacti
-        </v-sheet>
-
-        <v-row no-gutters>
-          <v-col cols="8">
-            <v-sheet class="pa-2 ma-2">
-              tipa name
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="4">
-            <v-sheet class="pa-2 ma-2">
-              tipa number
-            </v-sheet>
-          </v-col>
-        </v-row>
-
-        <v-row >
-          <v-col cols="8">
-            <v-sheet class="pa-2 ma-2">
-              tipa name
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="4">
-            <v-sheet class="pa-2 ma-2">
-              tipa number
-            </v-sheet>
-          </v-col>
-        </v-row>
-
-        <v-row >
-          <v-col cols="8">
-            <v-sheet class="pa-2 ma-2">
-              tipa name
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="4">
-            <v-sheet class="pa-2 ma-2">
-              tipa number
-            </v-sheet>
-          </v-col>
-        </v-row>
-
+  <H1 class="zagolovok">контакты</H1>
+  <div class="d-flex flex-column ma-4 align-start">
+    <v-row class="justify-center">
+      <v-col cols="12" md="8">
+        <p> <b>телефон: </b> +7 919 914 00 72</p>
+        <p> <b>почта: </b> wormyferma@gmail.com</p>
+        <p> <b>адрес: </b> Российская Федерация, Удмуртский регион, Якшур-Бодьинский район, село Якшур-Бодья, территория
+          Новая Земля, 1</p>
       </v-col>
-
-
     </v-row>
-  </v-container>
+  </div>
+
+  <H1 class="zagolovok"> сотрудники</H1>
+
+  <div class="d-flex justify-center flex-wrap">
+    <div class="d-flex flex-column ma-4 align-center">
+      <v-avatar icon="$vuetify" image="https://hightech.fm/wp-content/uploads/2021/09/shutterstock_1020065815.jpg"
+        size="250">
+      </v-avatar>
+      <h3 class="text-center">Березка Лев Сергеевич </h3>
+      <p class="text-caption mt-1 text-center">
+        прием заявок, куратор сайта, руководитель
+      </p>
+    </div>
+
+    <div class="d-flex flex-column ma-4 align-center">
+      <v-avatar icon="$vuetify" image="https://hightech.fm/wp-content/uploads/2021/09/shutterstock_1020065815.jpg"
+        size="250">
+      </v-avatar>
+      <h3 class="text-center">Золоторёв Николай </h3>
+      <p class="text-caption mt-1 text-center">
+        производственник, лаборант, руководитель
+      </p>
+    </div>
+
+    <div class="d-flex flex-column ma-4 align-center">
+      <v-avatar icon="$vuetify" image="https://hightech.fm/wp-content/uploads/2021/09/shutterstock_1020065815.jpg"
+        size="250">
+      </v-avatar>
+      <h3 class="text-center">муха </h3>
+      <p class="text-caption mt-1 text-center">
+        Контент-менеджер
+      </p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.zagolovok {
+  text-align: center;
+}
+</style>
