1a3b37e55098dac653280aa38c8a6035c7e72d0c
viktoria049 <viktoriagolovina109@gmail.com>
2023-10-27 17:18:13 +0400

    ProductCard

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,30 +1,25 @@
 
 <script setup>
+import { reactive } from "vue";
 import price from "../db/price.js"
-import { ref } from "vue"
-let selectPrice= ref()
+import ProductCard from "./ProductCard.vue";
+let cart= reactive([])
+function addToCart(args){
+cart.push(args)
+
+}
+
 
 
 </script>
 
 <template>
+    {{ cart }}
     <v-container>
         <v-row>
             <v-col v-for="product, index in price" :key="index">
-                <v-card class="mx-auto" width="344">
-                    <v-img :src="product.image" height="200px" cover></v-img>
-
-                    <v-card-title>
-                        {{ product.name }}
-                    </v-card-title>
-                    <v-card-subtitle class="pt-4">
-                        {{ product.discription }}
-                    </v-card-subtitle>
-                    <v-select label="Select" v-model="selectPrice" :items="product.prices" item-title="mass"
-                        item-value="price"></v-select>
-                        {{selectPrice}}
-
-                </v-card>
+              
+                <ProductCard :product="product" @addToCart="addToCart"/>
             </v-col>
         </v-row>
     </v-container>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import { ref } from "vue"
+let selectPrice = ref()
+const props = defineProps(['product'])
+ let emit =defineEmits(['addToCart'])
+function buy() {
+let productToCard={
+    name:props.product.name,
+    price: selectPrice.value
+}
+    emit('addToCart', productToCard)
+
+}
+
+</script>
+<template>
+    <v-card class="mx-auto" width="344">
+        <v-img :src="product.image" height="200px" cover></v-img>
+
+        <v-card-title>
+            {{ product.name }}
+        </v-card-title>
+        <v-card-subtitle class="pt-4">
+            {{ product.discription }}
+        </v-card-subtitle>
+        <v-select label="Select" v-model="selectPrice" :items="product.prices" item-title="mass"
+            item-value="price"></v-select>
+        {{ selectPrice }}
+        <div class="d-flex justify-center">
+
+            <v-btn @click="buy()" class="ma-4" color="indigo-darken-3">Купить</v-btn>
+
+
+        </div>
+    </v-card>
+</template>
